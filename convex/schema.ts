6bdc1051419b0b14c72c776a05cf82b395ef0b46
Prudent Bird <prudentbird@gmail.com>
2026-04-29 10:35:41 +0100

    feat: switch to better auth (#40)
    * refactor: migrate authentication from Clerk to Better Auth with Convex integration
    
    * [autofix.ci] apply automated fixes
    
    * feat: add .env example
    
    * fix: build error
    
    * chore: remove clerk module and integrate betterAuth component into api definition
    
    * chore: add dependencies for date handling and UI components
    
    * fix: build error
    
    * feat: add onboarding flow and fix auth errors
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Joseph Owonvwon <joseph.owonwo@gmail.com>

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,4 +1,3 @@
-import { authTables } from "@convex-dev/auth/server";
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
@@ -29,14 +28,13 @@ const project_schema = v.array(
 );
 
 const schema = defineSchema({
-  users: authTables.users,
   titles: defineTable({
     name: v.string(),
     description: v.nullable(v.string()),
     color: v.optional(v.string()),
   }),
   profile: defineTable({
-    userId: v.optional(v.id("users")),
+    userId: v.optional(v.string()),
     firstName: v.string(),
     lastName: v.string(),
     email: v.string(),
